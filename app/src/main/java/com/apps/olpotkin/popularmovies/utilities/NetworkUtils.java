f6a72fd1a583dfed735b26cd4451e58fa316a80a
Oleg Potkin <olpotkin@gmail.com>
2018-03-11 19:08:35 +0100

    feat: Implement getResponseFromHttpUrl method

--- app/src/main/java/com/apps/olpotkin/popularmovies/utilities/NetworkUtils.java
+++ app/src/main/java/com/apps/olpotkin/popularmovies/utilities/NetworkUtils.java
@@ -4,8 +4,12 @@ package com.apps.olpotkin.popularmovies.utilities;
 import android.net.Uri;
 import android.util.Log;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Scanner;
 
 public final class NetworkUtils {
 
@@ -13,7 +17,7 @@ public final class NetworkUtils {
     private static final String MOVIE_URL = "http://api.themoviedb.org/3/movie";
 
     // API key to access http://api.themoviedb.org/
-    private static final String API_KEY = "YOUR_API_KEY";
+    private static final String API_KEY = "PASTE_YOUR_API_KEY_HERE";
 
     final static String API_KEY_PARAM = "api_key";
 
@@ -33,5 +37,24 @@ public final class NetworkUtils {
         Log.v(TAG, "Built URI " + url);
 
         return url;
+    }
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
     }
 }
