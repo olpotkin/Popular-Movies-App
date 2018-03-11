8db98786cb0111972b6b40f5fadc7da30f63c9be
Oleg Potkin <olpotkin@gmail.com>
2018-03-11 19:50:43 +0100

    feat: Create data model for movie

---
+++ app/src/main/java/com/apps/olpotkin/popularmovies/model/Movie.java
@@ -0,0 +1,66 @@
+package com.apps.olpotkin.popularmovies.model;
+
+
+public class Movie {
+
+    private String title;
+    private String overview;
+    private String posterPath;
+    private String releaseDate;
+    private String voteAverage;
+
+    /**
+     * No args constructor for use in serialization
+     */
+    public Movie() {
+
+    }
+
+    public Movie(String title, String overview, String posterPath, String releaseDate, String voteAverage) {
+        this.title = title;
+        this.overview = overview;
+        this.posterPath = posterPath;
+        this.releaseDate = releaseDate;
+        this.voteAverage = voteAverage;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(String voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+}
