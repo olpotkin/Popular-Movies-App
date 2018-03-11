220edc887e3e844b7c4326fac0f80162fceee752
Oleg Potkin <olpotkin@gmail.com>
2018-03-11 12:21:26 +0100

    feat: Implement main menu

--- app/src/main/java/com/apps/olpotkin/popularmovies/MainActivity.java
+++ app/src/main/java/com/apps/olpotkin/popularmovies/MainActivity.java
@@ -1,13 +1,54 @@
 package com.apps.olpotkin.popularmovies;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    String active_category = "popular";                 // Active category
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        processGridView(active_category);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.option_mostpopular:
+                active_category = "popular";
+                processGridView(active_category);
+                return true;
+            case R.id.option_highestrated:
+                active_category = "top_rated";
+                processGridView(active_category);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void processGridView(String category) {
+
+    }
+
 }
